e60d26206af81da6617b8f8d5fe9769440d1b3cc
corvance <corvance24@gmail.com>
2022-03-05 13:10:10 +0000

    Simplify project file header

--- include/ox_common.h
+++ include/ox_common.h
@@ -1,23 +1,23 @@
-//------------------------------------------------------------------------------//
-//                                                                              //
-//  OxideNDS - A 2D + 3D Game/Application Engine for the Nintendo DS (NTR),     //
-//  built on devkitARM and libnds.                                              //
-//                                                                              //
-//  Copyright (C) 2022 - 2022 Conaer Macpherson a.k.a Corvance                  //
-//                                                                              //
-//  https://www.github.com/Corvance/oxide-nds                                   //
-//                                                                              //
-//------------------------------------------------------------------------------//
-
-//------------------------------------------------------------------------------//
-//                                                                              //
-//  OxideNDS is licensed under the terms of the MIT License, available at       //
-//  the file LICENSE in the root of this Git repository.                        //
-//                                                                              //
-//  If you discover any bugs, issues or room for improvement, open an           //
-//  issue.                                                                      //
-//                                                                              //
-//------------------------------------------------------------------------------//
+// ┌────────────────────────────────────────────────────────────────────────────┐
+// |                                                                            |
+// |   ██████╗ ██╗  ██╗██╗██████╗ ███████╗███╗   ██╗██████╗ ███████╗            |
+// |  ██╔═══██╗╚██╗██╔╝██║██╔══██╗██╔════╝████╗  ██║██╔══██╗██╔════╝            |
+// |  ██║   ██║ ╚███╔╝ ██║██║  ██║█████╗  ██╔██╗ ██║██║  ██║███████╗            |
+// |  ██║   ██║ ██╔██╗ ██║██║  ██║██╔══╝  ██║╚██╗██║██║  ██║╚════██║            |
+// |  ╚██████╔╝██╔╝ ██╗██║██████╔╝███████╗██║ ╚████║██████╔╝███████║            |
+// |   ╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝            |
+// |                                                                            |
+// |  OxideNDS - A 2D + 3D Game/Application Engine for the Nintendo DS (NTR),   |
+// |  built on devkitARM and libnds.                                            |
+// |                                                                            |
+// |  Copyright (C) 2022 - 2022 Conaer Macpherson a.k.a Corvance                |
+// |  OxideNDS is licensed under the terms of the MIT License.                  |
+// |                                                                            |
+// |  If you discover any bugs, issues or room for improvement, open an issue.  |
+// |                                                                            |
+// |  https://www.github.com/corvance/oxide-nds                                 |
+// |                                                                            |
+// └────────────────────────────────────────────────────────────────────────────┘
 
 /**
  * @file ox_common.h
